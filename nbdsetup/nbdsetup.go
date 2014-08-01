24d9888d625d9af5c3cf660bf46d1e349503c296
Andreas Klauer <Andreas.Klauer@metamorpher.de>
2014-08-01 20:46:20 +0200

    nbdsetup

---
+++ nbdsetup/nbdsetup.go
@@ -0,0 +1,28 @@
+// Copyright (C) 2014 Andreas Klauer <Andreas.Klauer@metamorpher.de>
+// License: GPL
+
+// nbdsetup is an alternative to losetup using network block devices.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/frostschutz/nbd"
+)
+
+func main() {
+	file := flag.String("file", "", "regular file or block device")
+	flag.Parse()
+	fmt.Printf("Using %s\n", *file)
+	device, err := os.Open(*file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	stat, _ := device.Stat()
+	nbd.Create(device, stat.Size())
+}
+
+// End of file.
